0972739bc9aa29809253ae9c66de3e18e422295a
Weed@2021 <geargym-steroids@yandex.com>
2021-11-22 20:24:48 -0800

    api link added

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[0];
+    const token = req.headers.authorization.split(" ")[1];
     // console.log(token);
     const decodedToken = jwt.verify(token, "secret_this_should_be_longer");
     //the following request field is a user created request field permitted by express which passes the field to the next execution scope
